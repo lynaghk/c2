cece85ed55448c9183848ec68b9d8f228d245db6
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-01 17:23:41 -0800

    Add axis label option.

--- src/cljs/c2/svg.cljs
+++ src/cljs/c2/svg.cljs
@@ -3,14 +3,24 @@
   (:require [pinot.dom :as dom]
             [pinot.html :as html]))
 
-(defn axis [selector scale & {:keys [ticks]}]
+(defn axis [selector scale & {:keys [ticks label label-sep]}]
     (unify! selector ticks
           (fn [d]
             [:svg:g.tick (translate (scale d) 0)
              [:svg:text {:y 30} (str d)]
              [:svg:line {:y1 0, :y2 10}]]))
     
-  ;;horizontal rule
-  (dom/append (first (dom/query selector))
-              (html/html [:svg:line.rule {:x1 (scale (first ticks))
-                                          :x2 (scale (last ticks))}])))
+    ;;Extra axis trimmings
+    (let [container (first (dom/query selector))
+          x1 (scale (first ticks))
+          x2 (scale (last ticks))]
+      
+      ;;horizontal rule
+      (dom/append container
+                  (html/html [:svg:line.rule {:x1 x1
+                                              :x2 x2}]))
+
+      (when label
+        (dom/append container
+                    (html/html [:svg:text.label {:x (+ x1 (/ (- x2 x1) 2))
+                                                 :y (or label-sep 50)} label])))))
