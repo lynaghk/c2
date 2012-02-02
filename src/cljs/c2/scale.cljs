7fb4dbb12ad38e4afba45c472dabaa310d565893
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-01 17:23:46 -0800

    Add log (base 10) scale.

--- src/cljs/c2/scale.cljs
+++ src/cljs/c2/scale.cljs
@@ -10,3 +10,13 @@
          (* range-length
             (/ (- x (first domain))
                domain-length))))))
+
+
+(defn log [& {:keys [domain range]
+              :or {domain [1 10]
+                   range  [0 1]}}]
+  (let [log #(/ (.log js/Math %)
+                (.-LN10 js/Math))
+        lin (linear :domain (map log domain)
+                    :range range)]
+    (comp lin log)))
