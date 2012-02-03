3c181955f7589862432162a29b30fbb1c5814a68
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-02 22:08:58 -0800

    Fix project.clj typo.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  
                  
                  [goog-jar "1.0.0"] ;;Required for C2 because of its Pinot submodule dependency; will drop when new Pinot JAR is cut or C2 stops using Pinot all together.
-                 [clojure.core.match.js :only [match]]
+                 [org.clojure/core.match "0.2.0-alpha9"]
                  [clj-iterate "0.95-SNAPSHOT"]]
   
   :dev-dependencies [[midje "1.3.1"]])
