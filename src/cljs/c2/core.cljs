lynaghk/c2

commit db03ceaa4db380d4bee4814b6fbf3961a68faf4a

Remove debug logging from (unify!).

src/cljs/c2/core.cljs
@@ -153,14 +153,14 @@ Optional enter, update, and exit functions called before DOM is changed; return 
   [container data mapping & {:keys [selector key-fn post-fn update exit enter]
                              :or {key-fn (fn [d idx] idx)
                                   enter  (fn [d idx new-node]
-                                           (p "no-op enter called")
+                                           #_(p "no-op enter called")
                                            new-node)
                                   update (fn [d idx old-node new-node]
                                            (merge-dom old-node (cannonicalize new-node))
-                                           (p "no-op update called")
+                                           #_(p "no-op update called")
                                            nil)
                                   exit   (fn [d idx old-node]
-                                           (p "default remove called"))}}]
+                                           #_(p "default remove called"))}}]
 
   (let [container (select container)
         ;;This logic should be abstracted out via a (unify!) multimethod, once (apply multimethod) is fixed in ClojureScript

commit 23fd36c9a06a159ec37b5ac065b8eb6496da0718

Fix default exit bug introduced in db03ceaa.

src/cljs/c2/core.cljs
@@ -160,7 +160,8 @@ Optional enter, update, and exit functions called before DOM is changed; return 
                                            #_(p "no-op update called")
                                            nil)
                                   exit   (fn [d idx old-node]
-                                           #_(p "default remove called"))}}]
+                                           #_(p "default remove called")
+                                           true)}}]
 
   (let [container (select container)
         ;;This logic should be abstracted out via a (unify!) multimethod, once (apply multimethod) is fixed in ClojureScript