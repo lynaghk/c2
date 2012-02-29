9fce46491b62ed2c81abc97702a470b84e68fb64
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-28 19:23:24 -0800

    Use vectorized math macro modified from Incanter.

--- src/clj/c2/maths.clj
+++ src/clj/c2/maths.clj
@@ -19,10 +19,35 @@
 (defn percentage [x y]
   (str (* 100 (/ x y)) "%"))
 
+
+
+;;element-by-element arithmetic
+;;Code modified from Incanter
+;;TODO move macros to their own namespace for CLJS compatability
+(defmacro combine-with [A B op fun]
+  `(cond
+    (and (number? ~A) (number? ~B)) (~op ~A ~B)
+    (and (coll? ~A) (coll? ~B)) (map ~op ~A ~B)
+    (and (number? ~A) (coll? ~B)) (map ~op (replicate (count ~B) ~A)  ~B)
+    (and (coll? ~A) (number? ~B)) (map ~op ~A (replicate (count ~A) ~B))))
+
+(defn add
+  ([& args] (reduce (fn [A B] (combine-with A B clojure.core/+ add)) args)))
+(defn sub
+  ([& args] (if (= (count args) 1)
+              (combine-with 0 (first args) clojure.core/- sub)
+              (reduce (fn [A B] (combine-with A B clojure.core/- sub)) args))))
+(defn mul
+  ([& args] (reduce (fn [A B] (combine-with A B clojure.core/* mul)) args)))
+(defn div
+  ([& args] (if (= (count args) 1)
+              (combine-with 1 (first args) clojure.core// div)
+              (reduce (fn [A B] (combine-with A B clojure.core// div)) args))))
+
 ;;CLJS!
 (comment
   (def sin (.-sin js/Math))
   (def cos (.-cos js/Math))
   (def expt (.-pow js/Math))
   (def sqrt (.-sqrt js/Math))
-)
+  )
