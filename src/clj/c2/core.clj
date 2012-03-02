0f56626e5ea67bb6ee1d80dfaf461a8ee3620791
Kevin J. Lynagh <kevin@keminglabs.com>
2012-03-01 21:33:25 -0800

    Add CLJ style helper.

--- src/clj/c2/core.clj
+++ src/clj/c2/core.clj
@@ -1,7 +1,15 @@
-(ns c2.core)
+(ns c2.core
+  (:use [clojure.string :only [join]]))
 
 (defn unify
   ([data mapping]
      (map mapping data))
   ([container data mapping]
       (into container (unify data mapping))))
+
+
+(defn style
+  "Convert map to CSS string"
+  [m]
+  (join (for [[k v] m]
+          (str (name k) ":" v ";"))))
