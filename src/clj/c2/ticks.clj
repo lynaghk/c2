d34f14e5b2f03290d487580b7a424f7bdf0831aa
Kevin J. Lynagh <kevin@keminglabs.com>
2012-03-08 09:07:13 -0800

    WIP: Placeholder for tick generation algorithm.

---
+++ src/clj/c2/ticks.clj
@@ -0,0 +1,8 @@
+(ns c2.ticks)
+;;Implementation of "An Extension of Wilkinson’s Algorithm for Positioning Tick Labels on Axes" by Justin Talbot, Sharon Lin, and Pat Hanrahan:
+;;
+;;    http://graphics.stanford.edu/vis/publications/2010/labeling-preprint.pdf
+;;
+;;see also: http://www.justintalbot.com/research/axis-labeling/
+
+
