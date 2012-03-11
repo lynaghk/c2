f0a93d07181828c3af3873553de4501e1cbb26b4
Kevin J. Lynagh <kevin@keminglabs.com>
2012-03-11 12:42:05 -0700

    Add bars example.

---
+++ vrepl/samples/bars.clj
@@ -0,0 +1,17 @@
+(ns bars
+  (:use [c2.core :only [unify style]]
+        [c2.maths :only [extent]])
+  (:require [c2.scale :as scale]))
+
+
+(let [width 500, bar-height 20
+      data {"A" 1, "B" 2, "C" 4, "D" 3}
+      s (scale/linear :domain [0 (apply max (vals data))]
+                      :range [0 width])]
+
+  [:div#bars
+   (unify data (fn [[label val]]
+                 [:div {:style (style {:height bar-height
+                                       :width (s val)
+                                       :background-color "gray"})}
+                  [:span {:style "color: white;"} label]]))])
