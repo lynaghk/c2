c56eee2ac92bef87fe0934aba28be6d689bb0f81
Kevin J. Lynagh <kevin@keminglabs.com>
2012-03-12 22:00:43 -0700

    Add :float option (default true) to linear scale that coerces scale output to float (html/svg doesn't care for ratios).

--- src/clj/c2/scale.clj
+++ src/clj/c2/scale.clj
@@ -3,15 +3,19 @@
         [c2.maths :only [log10]]))
 
 (c2-obj linear {:domain [0 1]
-                :range  [0 1]}
+                :range  [0 1]
+                :float true}
 
         clojure.lang.IFn
         (invoke [_ x] (let [domain-length (- (last domain) (first domain))
-                            range-length (- (last range) (first range))]
-                        (+ (first range)
-                           (* range-length
-                              (/ (- x (first domain))
-                                 domain-length))))))
+                            range-length (- (last range) (first range))
+                            scale-val (+ (first range)
+                                         (* range-length
+                                            (/ (- x (first domain))
+                                               domain-length)))]
+                        (if float
+                          (clojure.core/float scale-val)
+                          scale-val))))
 
 (c2-obj log {:domain [1 10]
              :range  [0 1]}
