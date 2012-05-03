862b34730a89f84a4c0b7183a6b53b2f25b2584f
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-03 08:46:16 -0700

    Release beta2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.keminglabs/c2 "0.1.0-beta2-SNAPSHOT"
+(defproject com.keminglabs/c2 "0.1.0-beta2"
   :description "Declarative data visualization in Clojure(Script)."
   :url "http://keminglabs.com/c2/"
   :license {:name "BSD" :url "http://www.opensource.org/licenses/BSD-3-Clause"}
