dceff03c1a4389e649c86f2e70957cb68cc45a50
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-29 16:02:26 -0700

    Proxy to clj->js in singult (it's slightly faster; no point in having the same fn twice).

--- src/cljs/c2/util.cljs
+++ src/cljs/c2/util.cljs
@@ -1,26 +1,7 @@
-(ns c2.util)
+(ns c2.util
+  (:require [singult.core :as singult]))
 
-;;Taken from Mark McGranaghan
-;;http://mmcgrana.github.com/2011/09/clojurescript-nodejs.html
-(defn clj->js
-  "Recursively transforms ClojureScript maps into Javascript objects,
-   other ClojureScript colls into JavaScript arrays, and ClojureScript
-   keywords into JavaScript strings."
-  [x]
-  (cond
-   (string? x)  x
-   (keyword? x) (name x)
-   (map? x)     (reduce (fn [o [k v]]
-                          (let [key (clj->js k)]
-                            (when-not (string? key)
-                              (throw "Cannot convert; JavaScript map keys must be strings"))
-                            (aset o key (clj->js v))
-                            o))
-                        (js-obj) x)
-   (coll? x)    (apply array (map clj->js x))
-   :else x))
-
-
+(def clj->js singult/clj->js)
 
 (defn ->coll
   "Convert something into a collection, if it's not already."
