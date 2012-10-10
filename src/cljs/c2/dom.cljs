dee4bc33d60ff0dcd6c968182b568a12ec8ae4b8
Kevin J. Lynagh <kevin@keminglabs.com>
2012-10-10 12:18:51 -0700

    dom/classed! should return element.

--- src/cljs/c2/dom.cljs
+++ src/cljs/c2/dom.cljs
@@ -176,7 +176,8 @@
 (defn classed!
   "Add or remove `class` to element based on boolean `classed?`, returning element."
   [el class classed?]
-  (gclasses/enable (->dom el) (name class) classed?))
+  (gclasses/enable (->dom el) (name class) classed?)
+  el)
 
 ;;TODO: make these kind of shortcuts macros for better performance.
 (defn add-class! [el class] (classed! el class true))
